7b916c48f275d0f73e9677c002f113b1ca673de6
809902 <amanwankar07@gmail.com>
2023-01-09 12:35:18 +0530

    4

--- src/scenes/ourClasses/index.tsx
+++ src/scenes/ourClasses/index.tsx
@@ -1,4 +1,4 @@
-import { ClassType, SelectedPage } from "@/shared/types";
+import { ClassType, SelectedPage } from "../../shared/types";
 import image1 from "@/assets/image1.png";
 import image2 from "@/assets/image2.png";
 import image3 from "@/assets/image3.png";
